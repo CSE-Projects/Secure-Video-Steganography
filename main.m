5a1c566c7889b0c42218addcb164e004fc16aa99
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2018-03-25 20:33:36 +0530

    RBG to YUV function added

--- main.m
+++ main.m
@@ -1,21 +1,40 @@
-%=== Set directories to store images ===%
+%=== Set directories to store video frames ===%
 workingDir = 'frames';
 mkdir(workingDir);
 mkdir(workingDir,'imagesRGB');
 mkdir(workingDir,'imagesYCbCr');
+mkdir(workingDir,'imagesY');
+mkdir(workingDir,'imagesU');
+mkdir(workingDir,'imagesV');
 
 %=== Get video reader obj for the video ===%
 video = VideoReader('small.mp4');
 i = 1;
 
-%=== Convert each video frame into different formats and save frames in different formats ===%
+%=== Convert each video frame into different formats and store them ===%
 while hasFrame(video)
+    
+   % Read frame from the video %
    img = readFrame(video);
-   new = rgb2ycbcr(img);
+   % Get YCbCr format from inbuilt function %
+   YCbCr = rgb2ycbcr(img);
+   % Get YUV format from function defined in rgb2yuv.m %
+   % each frame's Y, U, V component to be stored separately %
+   [Y, U, V] = rgb2yuv(img);
+   % Naming for each frame %
    filename = [sprintf('%03d',i) '.jpg'];
+   
+   %=== Store the frames in their respective directories ===%
    fullname = fullfile(workingDir,'imagesRGB',filename);
    imwrite(img,fullname);
    fullname = fullfile(workingDir,'imagesYCbCr',filename);
-   imwrite(new,fullname);
+   imwrite(YCbCr,fullname);
+   fullname = fullfile(workingDir,'imagesY',filename);
+   imwrite(Y,fullname);
+   fullname = fullfile(workingDir,'imagesU',filename);
+   imwrite(U,fullname);
+   fullname = fullfile(workingDir,'imagesV',filename);
+   imwrite(V,fullname);
+   
    i = i+1;
 end
---
+++ rgb2yuv.m
@@ -0,0 +1,14 @@
+%rgb2yuv Convert RBG frame to separate Y, U, V components
+function [Y, U, V] = rgb2yuv( RGB )
+	% Get R, G, B components of the frame
+    R = RGB(:,:,1); 
+    G = RGB(:,:,2); 
+    B = RGB(:,:,3); 
+    % Conversion formula
+    Y = 0.299 * R + 0.587 * G + 0.114 * B; 
+    U = -0.14713 * R - 0.28886 * G + 0.436 * B; 
+    V = 0.615 * R - 0.51499 * G - 0.10001 * B; 
+    % imshow(Y);
+    % imshow(U);
+    % imshow(V);
+end
