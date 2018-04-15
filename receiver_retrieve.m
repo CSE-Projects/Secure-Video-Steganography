b1f0cc23b9944cab34bde9d56cbf59ff0e99438b
dibyadarshan <dibyadarshan.hota@gmail.com>
2018-04-15 14:31:27 +0530

    Hamming correction added

--- receiver_retrieve.m
+++ receiver_retrieve.m
@@ -4,10 +4,14 @@ construct_frames(video, workingDir, 'receiver_embeddedFramesRGB', 'framesY', 'fr
 
 scramble(key, workingDir, 'framesY', 'framesU', 'framesV');
 read_message = zeros(28000,1);
+r = zeros(1,7);
+ht = [1 0 0; 0 1 0; 0 0 1; 1 1 0; 0 1 1; 1 1 1; 1 0 1];
 count = 1;
 position = 1;
 image_count = 1;
+xor_key = 8;
 
+frameY = zeros(320,560);
 frameV = zeros(320,560);
 frameU = zeros(320,560);
 
@@ -22,6 +26,11 @@ for i= 1:28000
     end
     
     if position == 1
+        
+        filename = [sprintf('%d',image_count) '.bmp'];
+        fullnameY = fullfile(workingDir,'framesY',filename);
+        frameY = imread(fullnameY);
+        
         filename = [sprintf('%d',image_count) '.bmp'];
         fullnameV = fullfile(workingDir,'framesV',filename);
         frameV = imread(fullnameV);
@@ -31,13 +40,72 @@ for i= 1:28000
         frameU = imread(fullnameU);
     end
     
+    r(1,1)=xor(xor_key,frameY(3,(position-1)*3+1));
+%     r(1,1)=mod(r(1,1),2);
+    r(1,2)=xor(xor_key,frameY(3,(position-1)*3+2));
+%     r(1,2)=mod(r(1,2),2);
+    r(1,3)=xor(xor_key,frameY(3,(position-1)*3+3));
+%     r(1,3)=mod(r(1,3),2);
+    r(1,4)=xor(xor_key,frameU(3,(position-1)*2+1));
+%     r(1,4)=mod(r(1,4),2);
+    r(1,5)=xor(xor_key,frameU(3,(position-1)*2+2));
+%     r(1,5)=mod(r(1,5),2);
+    r(1,6)=xor(xor_key,frameV(3,(position-1)*2+1));
+%     r(1,6)=mod(r(1,6),2);
+    r(1,7)=xor(xor_key,frameV(3,(position-1)*2+2));
+%     r(1,7)=mod(r(1,7),2);
+    
+    z = r * ht;
+
+    z(1,1)=mod(z(1,1),2);
+    z(1,2)=mod(z(1,2),2);
+    z(1,3)=mod(z(1,3),2);
+    
+    if (z(1,1)==0) && (z(1,2)==0) && (z(1,3)==0)
+        read_message(count)  =r(1,4);
+        read_message(count+1)=r(1,5);
+        read_message(count+2)=r(1,6);
+        read_message(count+3)=r(1,7);
     
     
-    read_message(count,1)=~frameU(3,(position-1)*2+1);
-    read_message(count+1,1)=~frameU(3,(position-1)*2+2);
+    elseif (z(1,1)==1) && (z(1,2)==1) && (z(1,3)==0)
+        read_message(count)  =~r(1,4);
+        read_message(count+1)=r(1,5);
+        read_message(count+2)=r(1,6);
+        read_message(count+3)=r(1,7);
     
-    read_message(count+2,1)=~frameV(3,(position-1)*2+1);
-    read_message(count+3,1)=~frameV(3,(position-1)*2+2);
+    
+    elseif (z(1,1)==0) && (z(1,2)==1) && (z(1,3)==1)
+        read_message(count)  =r(1,4);
+        read_message(count+1)=~r(1,5);
+        read_message(count+2)=r(1,6);
+        read_message(count+3)=r(1,7);
+    
+    
+    elseif (z(1,1)==1) && (z(1,2)==1) && (z(1,3)==1)
+        read_message(count)  =r(1,4);
+        read_message(count+1)=r(1,5);
+        read_message(count+2)=~r(1,6);
+        read_message(count+3)=r(1,7);
+    
+    
+    elseif (z(1,1)==1) && (z(1,2)==0) && (z(1,3)==1)
+        read_message(count)  =r(1,4);
+        read_message(count+1)=r(1,5);
+        read_message(count+2)=r(1,6);
+        read_message(count+3)=~r(1,7);
+    end
+    
+%     read_message(count,1)=xor(xor_key,frameU(3,(position-1)*2+1));
+%     read_message(count+1,1)=xor(xor_key,frameU(3,(position-1)*2+2));
+%     
+%     read_message(count+2,1)=xor(xor_key,frameV(3,(position-1)*2+1));
+%     read_message(count+3,1)=xor(xor_key,frameV(3,(position-1)*2+2));
+%     
+%     read_message(count,1)=mod(read_message(count,1),2);
+%     read_message(count+1,1)=mod(read_message(count+1,1),2);
+%     read_message(count+2,1)=mod(read_message(count+2,1),2);
+%     read_message(count+3,1)=mod(read_message(count+3,1),2);
     
     count = count + 4;
     position = position + 1;
